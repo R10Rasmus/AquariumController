e3f822de7e2c05c7c5418b28954adeec4c89b5e2
Rasmus Bang Nielsen <rbn@addanmark.dk>
2020-06-28 12:17:53 +0200

    first code

---
+++ AquariumController/Program.cs
@@ -0,0 +1,218 @@
+﻿using Lcd1602Controller;
+using System;
+using System.Threading;
+
+namespace AquariumController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (Lcd1602 lcd = new Lcd1602(registerSelectPin: 20, enablePin: 21, dataPins: new int[] { 26, 19, 13, 06 }, shouldDispose: true))
+            {
+                LcdConsole console = new LcdConsole(lcd, "A00", false)
+                {
+                    LineFeedMode = LineWrapMode.Wrap,
+                    ScrollUpDelay = new TimeSpan(0, 0, 1)
+                };
+
+                FishCharacters(lcd);
+                lcd.SetCursorPosition(0, 0);
+
+                int _fishCount = 0; 
+                bool _revers = false;
+                int _positionCount = 0;
+
+                while (!Console.KeyAvailable)
+                {
+                    console.ReplaceLine(0, "Tempertur er 9");
+                    ShowFishOnLine2(console, ref _fishCount, ref _revers, ref _positionCount);
+
+                }
+             
+                console.Dispose();
+
+            }
+
+            //int pinOut = 4;
+            //int pinIn = 21;
+
+            //GpioController controller = new GpioController();
+            //controller.OpenPin(pinOut, PinMode.Output);
+            //controller.OpenPin(pinIn, PinMode.InputPullDown);
+
+            //while (!Console.KeyAvailable)
+            //{
+            //    if(controller.Read(pinIn) == PinValue.High)
+            //    {
+            //        Console.WriteLine("on!");
+            //        controller.Write(pinOut, PinValue.High);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("off!");
+            //        controller.Write(pinOut, PinValue.Low);
+            //    }
+            //}
+
+            //controller.Dispose();
+        }
+
+        private static void ShowFishOnLine2(LcdConsole console, ref int _fishCount, ref bool _revers, ref int _positionCount)
+        {
+            string tmp = ((char)_fishCount).ToString();
+            for (int i = 0; i < _positionCount; i++)
+            {
+                tmp = " " + tmp;
+            }
+            console.ReplaceLine(1, tmp);
+            Thread.Sleep(1000);
+            if (_fishCount == 5)
+            {
+                _revers = true;
+            }
+            if (_fishCount == 0)
+            {
+                _revers = false;
+            }
+
+            if (!_revers)
+            {
+                _fishCount++;
+            }
+            else
+            {
+                _fishCount--;
+            }
+
+            if (_positionCount < 15)
+            {
+                _positionCount++;
+            }
+            else
+            {
+                _positionCount = 0;
+            }
+        }
+
+        //private static byte[] hartCharacter()
+        //{
+        //    byte[] arr = new byte[8];
+        //    arr[0] = 0b00000;
+        //    arr[1] = 0b01010;
+        //    arr[2] = 0b11111;
+        //    arr[3] = 0b11111;
+        //    arr[4] = 0b11111;
+        //    arr[5] = 0b01110;
+        //    arr[6] = 0b00100;
+        //    arr[7] = 0b00000;
+
+        //    return arr;
+        //}
+
+
+        //private static void CreateWalkCharacters(Lcd1602.Lcd1602 lcd)
+        //{
+        //    byte[] Walk1 = new byte[8];
+        //    Walk1[0] = 0b_00110;
+        //    Walk1[1] = 0b_00110;
+        //    Walk1[2] = 0b_01100;
+        //    Walk1[3] = 0b_10111;
+        //    Walk1[4] = 0b_00100;
+        //    Walk1[5] = 0b_01110;
+        //    Walk1[6] = 0b_01010;
+        //    Walk1[7] = 0b_10001;
+
+        //    byte[] Walk2 = new byte[8];
+        //    Walk2[0] = 0b_00110;
+        //    Walk2[1] = 0b_00110;
+        //    Walk2[2] = 0b_01100;
+        //    Walk2[3] = 0b_01100;
+        //    Walk2[4] = 0b_00110;
+        //    Walk2[5] = 0b_00110;
+        //    Walk2[6] = 0b_01010;
+        //    Walk2[7] = 0b_01010;
+
+        //    // Walk 1
+        //    lcd.CreateCustomCharacter(0,
+        //       Walk1);
+        //    // Walk 2
+        //    // Walk 1
+        //    lcd.CreateCustomCharacter(1,
+        //       Walk2);
+        //}
+
+        private static void FishCharacters(Lcd1602 lcd)
+        {
+            byte[] Fish1 = new byte[8];
+            Fish1[0] = 0b_00000;
+            Fish1[1] = 0b_00000;
+            Fish1[2] = 0b_00000;
+            Fish1[3] = 0b_00000;
+            Fish1[4] = 0b_00000;
+            Fish1[5] = 0b_10110;
+            Fish1[6] = 0b_11001;
+            Fish1[7] = 0b_10110;
+
+            byte[] Fish2 = new byte[8];
+            Fish2[0] = 0b_00000;
+            Fish2[1] = 0b_00000;
+            Fish2[2] = 0b_00000;
+            Fish2[3] = 0b_00000;
+            Fish2[4] = 0b_10110;
+            Fish2[5] = 0b_11001;
+            Fish2[6] = 0b_00110;
+            Fish2[7] = 0b_00000;
+
+            byte[] Fish3 = new byte[8];
+            Fish3[0] = 0b_00000;
+            Fish3[1] = 0b_00000;
+            Fish3[2] = 0b_00000;
+            Fish3[3] = 0b_00110;
+            Fish3[4] = 0b_11001;
+            Fish3[5] = 0b_10110;
+            Fish3[6] = 0b_00000;
+            Fish3[7] = 0b_00000;
+            byte[] Fish4 = new byte[8];
+            Fish4[0] = 0b_00000;
+            Fish4[1] = 0b_00000;
+            Fish4[2] = 0b_10110;
+            Fish4[3] = 0b_11001;
+            Fish4[4] = 0b_10110;
+            Fish4[5] = 0b_00000;
+            Fish4[6] = 0b_00000;
+            Fish4[7] = 0b_00000;
+
+            byte[] Fish5 = new byte[8];
+            Fish5[0] = 0b_00000;
+            Fish5[1] = 0b_00110;
+            Fish5[2] = 0b_11001;
+            Fish5[3] = 0b_10110;
+            Fish5[4] = 0b_00000;
+            Fish5[5] = 0b_00000;
+            Fish5[6] = 0b_00000;
+            Fish5[7] = 0b_00000;
+
+            byte[] Fish6 = new byte[8];
+            Fish6[0] = 0b_10110;
+            Fish6[1] = 0b_11001;
+            Fish6[2] = 0b_00110;
+            Fish6[3] = 0b_00000;
+            Fish6[4] = 0b_00000;
+            Fish6[5] = 0b_00000;
+            Fish6[6] = 0b_00000;
+            Fish6[7] = 0b_00000;
+
+            lcd.CreateCustomCharacter(0,Fish1);
+            lcd.CreateCustomCharacter(1, Fish2);
+            lcd.CreateCustomCharacter(2, Fish3);
+            lcd.CreateCustomCharacter(3, Fish4);
+            lcd.CreateCustomCharacter(4, Fish5);
+            lcd.CreateCustomCharacter(5, Fish6);
+        }
+
+     
+    }
+}
---
+++ Lcd1602Controller/LcdInterface.I2c.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.I2c;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lcd1602Controller
+{
+    public abstract partial class LcdInterface : IDisposable
+    {
+        /// <summary>
+        /// Built-in I2c access to the Hd44780 compatible controller. The Philips/NXP LCD driver ICs
+        /// (such as the PCF2119x) are examples of this support.
+        /// </summary>
+        private class I2c : LcdInterface
+        {
+            [Flags]
+            private enum ControlByteFlags : byte
+            {
+                /// <summary>
+                /// When set, another control byte will follow the next data/command byte.
+                /// Otherwise the last control byte will be used.
+                /// </summary>
+                /// <remarks>
+                /// This is only relevant when sending multiple bytes of data to the register.
+                /// When a new I2c transmission is made, the first byte is always assumed to
+                /// be a control byte. This flag is needed if you want to flip the "RS" bit
+                /// in a stream of bytes.
+                /// </remarks>
+                ControlByteFollows = 0b_1000_0000,
+
+                /// <summary>
+                /// When set the data register will be selected (i.e. equivalent to
+                /// RS pin being high). Otherwise the instruction/command register
+                /// will be updated.
+                /// </summary>
+                RegisterSelect = 0b_0100_0000
+
+                // No other bits are used
+            }
+
+            // Philips created the I2c bus and likely were the first to provide character LCD
+            // controllers with I2c functionality built-in. This driver was written using the
+            // PCF2119x datasheet as reference. Other chipsets are compatible with the addressing
+            // scheme (Sitronix ST7036, Aiptek AIP31068L, probably others).
+            private readonly I2cDevice _device;
+
+            public I2c(I2cDevice device)
+            {
+                _device = device;
+
+                // While the LCD controller can be set to 4 bit mode there really isn't a way to
+                // mess with that from the I2c pins as far as I know. Other drivers try to set the
+                // controller up for 8 bit mode, but it appears they are doing so only because they've
+                // copied existing HD44780 drivers.
+            }
+
+            public override bool EightBitMode => true;
+
+            public override bool BacklightOn
+            {
+                get
+                {
+                    // Setting the backlight on or off is not supported with 8 bit commands, according to the docs.
+                    return true;
+                }
+                set
+                {
+                    // Ignore setting the backlight. Exceptions are not expected by user code here, as it is normal to
+                    // enable this during initialization, so that it is enabled whether switching it is supported or not.
+                }
+            }
+
+            public override void SendCommand(byte command)
+            {
+                Span<byte> buffer = stackalloc byte[]
+                {
+                    0x00,
+                    command
+                };
+                _device.Write(buffer);
+            }
+
+            public override void SendCommands(ReadOnlySpan<byte> commands)
+            {
+                // There is a limit to how much data the controller can accept at once. Haven't found documentation
+                // for this yet, can probably iterate a bit more on this to find a true "max". Not adding additional
+                // logic like SendData as we don't expect a need to send more than a handful of commands at a time.
+                if (commands.Length > 20)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(commands), "Too many commands in one request.");
+                }
+
+                Span<byte> buffer = stackalloc byte[commands.Length + 1];
+                buffer[0] = 0x00;
+                commands.CopyTo(buffer.Slice(1));
+                _device.Write(buffer);
+            }
+
+            public override void SendData(byte value)
+            {
+                Span<byte> buffer = stackalloc byte[]
+                {
+                    (byte)ControlByteFlags.RegisterSelect,
+                    value
+                };
+                _device.Write(buffer);
+            }
+
+            public override void SendData(ReadOnlySpan<byte> values)
+            {
+                // There is a limit to how much data the controller can accept at once. Haven't found documentation
+                // for this yet, can probably iterate a bit more on this to find a true "max". 40 was too much.
+                const int MaxCopy = 20;
+                Span<byte> buffer = stackalloc byte[MaxCopy + 1];
+                buffer[0] = (byte)ControlByteFlags.RegisterSelect;
+                Span<byte> bufferData = buffer.Slice(1);
+
+                while (values.Length > 0)
+                {
+                    ReadOnlySpan<byte> currentValues = values.Slice(0, values.Length > MaxCopy ? MaxCopy : values.Length);
+                    values = values.Slice(currentValues.Length);
+                    currentValues.CopyTo(bufferData);
+                    _device.Write(buffer.Slice(0, currentValues.Length + 1));
+                }
+            }
+        }
+    }
+}
